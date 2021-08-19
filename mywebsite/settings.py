d098d182e734f94eb32e4fe6e5f433cf171962d4
sam.sabellano <ssabellano851998@gmail.com>
2021-08-19 16:16:43 +0800

    fourth commit

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -131,9 +131,9 @@ USE_TZ = False
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
