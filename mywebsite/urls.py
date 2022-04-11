31eda0dffb2e3aa150989b6d5a23b09034af0e4c
sam.sabellano <ssabellano851998@gmail.com>
2022-04-11 20:14:24 +0800

    updated version

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import redirect_main_page
 
 urlpatterns = [
-    path('', redirect_main_page),
     path('admin/', admin.site.urls),
-    path('home/', include('mysite.urls', namespace='mysite'))
+    path('', include('mysite.urls', namespace='mysite'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mywebsite/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import redirect
-
-
-def redirect_main_page(request):
-    return redirect('mysite:main_page', permanent=True)
