20d75403d96e5b2f62e86f081e3b296a8cb4c8c9
sam.sabellano <ssabellano851998@gmail.com>
2022-04-12 22:38:48 +0800

    7th commit

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', include('mysite.urls', namespace='mysite'))
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += (
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    )
